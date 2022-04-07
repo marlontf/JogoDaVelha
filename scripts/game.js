bc89c5aa91edd88ca3e0fbb01c2d81ae3e8e06af
Marlon Tavares <marlontf@gmail.com>
2022-04-06 22:08:57 -0300

    Add eventListen to set player symbol

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,19 @@
 //Iniciando as variáveis
-let board = ['','','','','','','','','']
+let board = ['', '', '', '', '', '', '', '', '']
 let playerTime = 0
-let symbols = ['o','x']
+let symbols = ['o', 'x']
+
+function handleMove(position) {
+
+   if (board[position] == '') {
+
+      board[position] = symbols[playerTime];
+
+      if (playerTime == 0) {
+         playerTime = 1
+      } else {
+         playerTime = 0
+      }
+   }
+
+}
--- scripts/interface.js
+++ scripts/interface.js
@@ -12,4 +12,26 @@ function handleClick(event) {
 
    console.log(event.target)
 
+   let square = event.target
+   let position = square.id
+
+   handleMove(position)
+   updateSquares()
+
+}
+
+function updateSquares() {
+
+   let squares = document.querySelectorAll('.square')
+
+   squares.forEach((square) => {
+      let position = square.id
+      let symbol = board[position]
+
+      if (symbol != '') {
+         square.innerHTML = `<div class='${symbol}'></div>`
+      }
+
+   })
+
 }
